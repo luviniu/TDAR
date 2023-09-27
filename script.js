6907dd2ebac4d0e5a7c657a47564f22e7102a48b
Luan Alves <110785695+luviniu@users.noreply.github.com>
2023-09-27 11:00:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const checkbox = document.getElementById('cee');
+const header = document.querySelector('header');
+const body = document.body;
+const lampadaImg = document.querySelector('.dia-noite-img');
+
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (prefersDarkMode) {
+  header.classList.add('dark-theme');
+  body.classList.add('dark-theme');
+  checkbox.checked = true;
+  lampadaImg.src = 'moon.png';
+}
+
+checkbox.addEventListener('change', function () {
+  if (this.checked) {
+    header.classList.add('dark-theme');
+    body.classList.add('dark-theme');
+    lampadaImg.src = 'moon.png';
+  } else {
+    header.classList.remove('dark-theme');
+    body.classList.remove('dark-theme');
+    lampadaImg.src = 'sun.png';
+  }
+});
